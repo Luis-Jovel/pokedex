517f9297b2b030731e689f08f756febf4412a23e
Luis <luigi.lsjp@gmail.com>
2016-08-04 21:29:45 -0600

    app.ts update

---
+++ app.ts
@@ -0,0 +1,91 @@
+var express 	= require('express');
+var path 		= require('path');
+var favicon		= require('serve-favicon');
+var bodyParser	= require('body-parser');
+var http 		= require('http');
+
+var app = express();
+var port = process.env.PORT || '3000';
+app.set('port', port);
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static(__dirname + '/node_modules'));
+// app.use(express.static(__dirname + '/index.html'));
+// app.use(express.static(path.join(__dirname, 'app')));
+app.use(express.static(__dirname));
+
+app.get('*', function(req, res){
+    res.sendFile( __dirname + '/index.html' );
+});
+
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+}
